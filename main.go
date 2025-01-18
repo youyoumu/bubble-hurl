dd351d56ff5f40a38edae37d9a26db1427f3cbe4
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-01-18 16:14:10 +0700

    Copy file-picker example

--- main.go
+++ main.go
@@ -1,125 +1,95 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"net/http"
 	"os"
+	"strings"
 	"time"
+
+	"github.com/charmbracelet/bubbles/filepicker"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-const url = "https://charm.sh/"
-
-type errMsg struct {
-	err error
-}
-
-func (e errMsg) Error() string {
-	return e.err.Error()
-}
-
-type statusMsg int
-
 type model struct {
-	choices  []string
-	cursor   int
-	selected map[int]struct{}
-	status   int
-	err      error
+	filepicker   filepicker.Model
+	selectedFile string
+	quitting     bool
+	err          error
 }
 
-func checkServer() tea.Msg {
-	c := &http.Client{Timeout: 10 * time.Second}
-	res, err := c.Get(url)
-	if err != nil {
-		return errMsg{err}
-	}
+type clearErrorMsg struct{}
 
-	return statusMsg(res.StatusCode)
-}
-
-func initialModel() model {
-	return model{
-		choices:  []string{"one", "two", "three"},
-		selected: make(map[int]struct{}),
-	}
+func clearErrorAfter(t time.Duration) tea.Cmd {
+	return tea.Tick(t, func(_ time.Time) tea.Msg {
+		return clearErrorMsg{}
+	})
 }
 
 func (m model) Init() tea.Cmd {
-	return checkServer
+	return m.filepicker.Init()
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-
-	case statusMsg:
-		m.status = int(msg)
-
-	case errMsg:
-		m.err = msg
-
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c", "q":
+			m.quitting = true
 			return m, tea.Quit
-		case "up", "k":
-			if m.cursor > 0 {
-				m.cursor--
-			}
-		case "down", "j":
-			if m.cursor < len(m.choices)-1 {
-				m.cursor++
-			}
-		case "enter", " ":
-			_, ok := m.selected[m.cursor]
-			if ok {
-				delete(m.selected, m.cursor)
-			} else {
-				m.selected[m.cursor] = struct{}{}
-			}
 		}
+	case clearErrorMsg:
+		m.err = nil
 	}
 
-	return m, nil
+	var cmd tea.Cmd
+	m.filepicker, cmd = m.filepicker.Update(msg)
+
+	// Did the user select a file?
+	if didSelect, path := m.filepicker.DidSelectFile(msg); didSelect {
+		// Get the path of the selected file.
+		m.selectedFile = path
+	}
+
+	// Did the user select a disabled file?
+	// This is only necessary to display an error to the user.
+	if didSelect, path := m.filepicker.DidSelectDisabledFile(msg); didSelect {
+		// Let's clear the selectedFile and display an error.
+		m.err = errors.New(path + " is not valid.")
+		m.selectedFile = ""
+		return m, tea.Batch(cmd, clearErrorAfter(2*time.Second))
+	}
+
+	return m, cmd
 }
 
 func (m model) View() string {
-	s := "hello world\n\n"
-
-	for i, choice := range m.choices {
-		cursor := " "
-
-		if m.cursor == i {
-			cursor = ">"
-		}
-
-		checked := " "
-		if _, ok := m.selected[i]; ok {
-			checked = "x"
-		}
-
-		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	if m.quitting {
+		return ""
 	}
-
+	var s strings.Builder
+	s.WriteString("\n  ")
 	if m.err != nil {
-		return fmt.Sprintf("\nWe had some trouble: %v\n\n", m.err)
+		s.WriteString(m.filepicker.Styles.DisabledFile.Render(m.err.Error()))
+	} else if m.selectedFile == "" {
+		s.WriteString("Pick a file:")
+	} else {
+		s.WriteString("Selected file: " + m.filepicker.Styles.Selected.Render(m.selectedFile))
 	}
-
-	s += fmt.Sprintf("Checking %s ... ", url)
-	if m.status > 0 {
-		s += fmt.Sprintf("%d %s!", m.status, http.StatusText(m.status))
-	}
-
-	s += "\nPress q to quit\n"
-
-	return s
+	s.WriteString("\n\n" + m.filepicker.View() + "\n")
+	return s.String()
 }
 
 func main() {
-	p := tea.NewProgram(initialModel())
-	if _, err := p.Run(); err != nil {
-		fmt.Printf("Error: %v", err)
-		os.Exit(1)
+	fp := filepicker.New()
+	fp.AllowedTypes = []string{".mod", ".sum", ".go", ".txt", ".md"}
+	fp.CurrentDirectory, _ = os.UserHomeDir()
+
+	m := model{
+		filepicker: fp,
 	}
+	tm, _ := tea.NewProgram(&m).Run()
+	mm := tm.(model)
+	fmt.Println("\n  You selected: " + m.filepicker.Styles.Selected.Render(mm.selectedFile) + "\n")
 }
