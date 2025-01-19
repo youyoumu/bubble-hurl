f6353dde8f8459a64f8eb6e34bb9d6d172a87284
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-01-19 18:33:57 +0700

    Scrollable hurl view

--- main.go
+++ main.go
@@ -48,14 +48,12 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	}
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
+		filePickerHeight := lipgloss.Height(m.filePickerView())
+		catHeight := lipgloss.Height(m.catView())
+
 		if !m.ready {
-			// Since this program is using the full size of the viewport we
-			// need to wait until we've received the window dimensions before
-			// we can initialize the viewport. The initial dimensions come in
-			// quickly, though asynchronously, which is why we wait for them
-			// here.
 			m.hurlViewport = viewport.New(msg.Width-2, 10)
-			// m.hurlViewport.YPosition = headerHeight
+			m.hurlViewport.YPosition = filePickerHeight + catHeight
 			m.ready = true
 		} else {
 			m.hurlViewport.Width = msg.Width - 2
@@ -83,37 +81,41 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		cmds []tea.Cmd
 	)
 
-	m.filepicker, cmd = m.filepicker.Update(msg)
+	m.hurlViewport.Style = borderStyle(m.activeWindow == 3)
+	m.hurlViewport.SetContent(m.hurlView())
 
-	// Did the user select a file?
-	if didSelect, path := m.filepicker.DidSelectFile(msg); didSelect {
-		// Get the path of the selected file.
-		m.selectedFile = path
-		if m.selectedFile == path {
-			hurlOutput, err := exec.Command("hurl", path, "--variables-file", "/home/yym/SSD-1TB/coding/repos/hurl/hurl.env").CombinedOutput()
-			if err == nil {
-				jqOutput, jqErr := exec.Command("jq", "--color-output", "--null-input", "--jsonargs", string(hurlOutput)).CombinedOutput()
-				if jqErr == nil {
-					m.hurlOutput = string(jqOutput)
+	switch m.activeWindow {
+	case 1:
+		m.filepicker, cmd = m.filepicker.Update(msg)
+		// Did the user select a file?
+		if didSelect, path := m.filepicker.DidSelectFile(msg); didSelect {
+			// Get the path of the selected file.
+			m.selectedFile = path
+			if m.selectedFile == path {
+				hurlOutput, err := exec.Command("hurl", path, "--variables-file", "/home/yym/SSD-1TB/coding/repos/hurl/hurl.env").CombinedOutput()
+				if err == nil {
+					jqOutput, jqErr := exec.Command("jq", "--color-output", "--null-input", "--jsonargs", string(hurlOutput)).CombinedOutput()
+					if jqErr == nil {
+						m.hurlOutput = string(jqOutput)
+					} else {
+						m.hurlOutput = jqErr.Error()
+					}
 				} else {
-					m.hurlOutput = jqErr.Error()
+					m.hurlOutput = err.Error() + "\n" + string(hurlOutput)
 				}
-			} else {
-				m.hurlOutput = err.Error() + "\n" + string(hurlOutput)
 			}
 		}
+
+		if didSelect, path := m.filepicker.DidSelectDisabledFile(msg); didSelect {
+			m.filePickerError = errors.New(path + " is not valid.")
+			m.selectedFile = ""
+			return m, tea.Batch(cmd, clearErrorAfter(2*time.Second))
+		}
+
+	case 3:
+		m.hurlViewport, cmd = m.hurlViewport.Update(msg)
 	}
 
-	// Did the user select a disabled file?
-	// This is only necessary to display an error to the user.
-	if didSelect, path := m.filepicker.DidSelectDisabledFile(msg); didSelect {
-		// Let's clear the selectedFile and display an error.
-		m.filePickerError = errors.New(path + " is not valid.")
-		m.selectedFile = ""
-		return m, tea.Batch(cmd, clearErrorAfter(2*time.Second))
-	}
-
-	// m.hurlViewport, cmd = m.hurlViewport.Update(msg)
 	cmds = append(cmds, cmd)
 
 	return m, tea.Batch(cmds...)
@@ -128,7 +130,7 @@ func (m model) View() string {
 	s.WriteString("\n")
 	s.WriteString(m.catView())
 	s.WriteString("\n")
-	s.WriteString(m.hurlView())
+	s.WriteString(m.hurlViewport.View())
 
 	return s.String()
 }
@@ -174,8 +176,7 @@ func (m model) hurlView() string {
 	s.WriteString("\n")
 	s.WriteString("\n")
 	s.WriteString(string(m.hurlOutput))
-	m.hurlViewport.SetContent(s.String())
-	return borderStyle(m.activeWindow == 3).Render(m.hurlViewport.View())
+	return s.String()
 }
 
 func main() {
@@ -199,7 +200,7 @@ func main() {
 		dump:         dump,
 		filepicker:   fp,
 	}
-	if _, err := tea.NewProgram(&m).Run(); err != nil {
+	if _, err := tea.NewProgram(&m, tea.WithMouseCellMotion()).Run(); err != nil {
 		fmt.Println("could not start program:", err)
 		os.Exit(1)
 	}
